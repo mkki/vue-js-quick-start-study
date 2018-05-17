2c009088f678c4c1d511b6c6643a63a06b66d106
mkki <gosorn1@gmail.com>
2018-05-17 13:59:15 +0900

    separate store by role

--- chapter11/todolistapp/src/store/index.js
+++ chapter11/todolistapp/src/store/index.js
@@ -1,45 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import Constant from '../constant';
+import state from './state';
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-        todolist: [
-            { todo: "영화", done: false },
-            { todo: "야구", done: false },
-            { todo: "공부", done: false },
-            { todo: "카페", done: false }
-        ]
-    },
-    mutations: {
-        [Constant.ADD_TODO]: (state, payload) => {
-            if (payload.todo !== "") {
-                state.todolist.push({ todo: payload.todo, done: false });
-            }
-        },
-        [Constant.DONE_TOGGLE]: (state, payload) => {
-            state.todolist[payload.index].done = !state.todolist[payload.index].done;
-        },
-        [Constant.DELETE_TODO]: (state, payload) => {
-            state.todolist.splice(payload.index, 1);
-        }
-    },
-    actions: {
-        [Constant.ADD_TODO]: ({ state, commit }, payload) => {
-            console.log("##addTodo");
-            commit(Constant.ADD_TODO, payload);
-        },
-        [Constant.DELETE_TODO]: ({ state, commit }, payload) => {
-            console.log("##deleteTodo");
-            commit(Constant.DELETE_TODO, payload);
-        },
-        [Constant.DONE_TOGGLE]: ({ state, commit }, payload) => {
-            console.log("##doneToggle");
-            commit(Constant.DONE_TOGGLE, payload);
-        }
-    }
+    state,
+    mutations,
+    actions
 })
 
 export default store;
---
+++ chapter11/todolistapp/src/store/state.js
@@ -0,0 +1,8 @@
+export default {
+    todolist: [
+        { todo: "영화", done: false },
+        { todo: "야구", done: false },
+        { todo: "공부", done: false },
+        { todo: "카페", done: false }
+    ]
+}
