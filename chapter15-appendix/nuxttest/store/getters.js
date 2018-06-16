a8efcdfe6b45128e56f5d71066bbe199ff820ac9
mkki <gosorn1@gmail.com>
2018-06-17 01:30:12 +0900

    add example code of chapter15

---
+++ chapter15-appendix/nuxttest/store/getters.js
@@ -0,0 +1,14 @@
+export default {
+    getContactOne(state) {
+        var no = state.no;
+        var arr = state.contacts.filter((item, index) => {
+            return item.no == no;
+        })
+
+        if (arr.length == 1) return arr[0];
+        else return {};
+    },
+    getContacts(state) {
+        return state.contacts;
+    }
+}
